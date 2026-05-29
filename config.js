6b725ff415b3eb774f1b98a3b266c2713032018b
oncloudnintynine <oncloudnintynine@gmail.com>
2026-05-29 22:52:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,9 @@
 // Instance-Specific Configuration
 // ==========================================
 const APP_CONFIG = {
-APP_NAME: "App Code Maintainer (MYG)",
-GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzf6zu4uxmqe_wC92ixIISm0LRtrOME5nUv94bB0tAd0dbzflLIUydvgE6ISe-qIZuZ/exec"
+APP_NAME: "App Code Maintainer",
+GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzf6zu4uxmqe_wC92ixIISm0LRtrOME5nUv94bB0tAd0dbzflLIUydvgE6ISe-qIZuZ/exec",
+TARGET_REPO: "oncloudnintynine/Fail-Safe-Code-Updater",
+TARGET_FOLDER_ID: "1X8bpO1kyeS5GmUZ2s5X2tbOnJ-6QjzhD",
+TARGET_BRANCH: "main"
 };
