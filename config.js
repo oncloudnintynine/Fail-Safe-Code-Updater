758988e0cbaaffa1cc7621eb88dfb9f949a6444b
oncloudnintynine <oncloudnintynine@gmail.com>
2026-05-29 22:39:48 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// ==========================================
+// Instance-Specific Configuration
+// ==========================================
+const APP_CONFIG = {
+APP_NAME: "App Code Maintainer (MYG)",
+GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzf6zu4uxmqe_wC92ixIISm0LRtrOME5nUv94bB0tAd0dbzflLIUydvgE6ISe-qIZuZ/exec"
+};
