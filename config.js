501ed08423ec32e4623fb4ce97bf7c3026531507
weronedatabase-tech <werone.database@gmail.com>
2026-06-05 02:54:59 +0800

    Add LINKS object to config.js

--- config.js
+++ config.js
@@ -6,5 +6,9 @@ APP_NAME: "App Code Maintainer",
 GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzf6zu4uxmqe_wC92ixIISm0LRtrOME5nUv94bB0tAd0dbzflLIUydvgE6ISe-qIZuZ/exec",
 TARGET_REPO: "oncloudnintynine/Fail-Safe-Code-Updater",
 TARGET_FOLDER_ID: "1X8bpO1kyeS5GmUZ2s5X2tbOnJ-6QjzhD",
-TARGET_BRANCH: "main"
+TARGET_BRANCH: "main",
+LINKS: {
+  TEMPLATE_PROMPT: "https://docs.google.com/document/d/1Fq5UK1ucjfZD9Aau0yP6-I-yPKyfL-l_5USWjggQURU/edit?usp=drive_link",
+  TKN: "https://docs.google.com/document/d/1XpIhmVrElmD4wd5z1qU70JW28g8V81vzgR1R2ELkJBo/edit?usp=drive_link"
+}
 };
